b6f11bf04c81410b3ab1dda076c974d521da86a2
Thiago Feldner <tfeldner@hotmail.com>
2023-08-17 17:37:59 -0300

    Criação do projeto organo usando react

---
+++ src/components/Time/index.js
@@ -0,0 +1,27 @@
+import Colaborador from "../Colaborador";
+import "./Time.css";
+
+const Time = (props) => {
+  const bgColor = { backgroundColor: props.corSecundaria };
+  const borderColor = { borderColor: props.corPrimaria };
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={bgColor}>
+      <h3 style={borderColor}> {props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            corDeFundo = {props.corPrimaria}
+          />
+        ))}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
+
+export default Time;
