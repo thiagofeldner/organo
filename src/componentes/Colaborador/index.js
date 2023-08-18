5f936f007dddf08e31b667e1b81020eac59a38fc
Thiago Feldner <tfeldner@hotmail.com>
2023-08-18 11:31:14 -0300

    inserindo icone de deletar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,9 +1,10 @@
+import { AiFillCloseCircle } from 'react-icons/ai';
 import "./colaborador.css";
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
   return (
     <div className="colaborador">
-      <div className="deletar" onClick={aoDeletar}>deletar</div>
+      <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
       <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
         <img src={colaborador.imagem} alt={colaborador.nome} />
       </div>
