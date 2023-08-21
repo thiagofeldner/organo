9675210124d4c1ff85e8d5131e33f381343c09b7
thiagofeldner <tfeldner@hotmail.com>
2023-08-21 15:14:09 -0300

    Alteração nos nomes dos times

--- src/App.js
+++ src/App.js
@@ -15,32 +15,32 @@ function App() {
     },
     {
       id: uuidv4(),
-      nome: "Front-End",
+      nome: "Licitação",
       cor: "#82CFFA",
     },
     {
       id: uuidv4(),
-      nome: "Data Science",
+      nome: "Projetos",
       cor: "#A6D157",
     },
     {
       id: uuidv4(),
-      nome: "Devops",
+      nome: "Integração",
       cor: "#E06B69",
     },
     {
       id: uuidv4(),
-      nome: "UX e Design",
+      nome: "Jurídico",
       cor: "#DB6EBF",
     },
     {
       id: uuidv4(),
-      nome: "Mobile",
+      nome: "Gerência",
       cor: "#FFBA05",
     },
     {
       id: uuidv4(),
-      nome: "Inovação e Gestão",
+      nome: "Diretor",
       cor: "#FF8A29",
     },
   ]);
@@ -85,100 +85,19 @@ function App() {
     {
       id: uuidv4(),
       favorito: false,
-      nome: "DANIEL ARTINE",
-      cargo: "Engenheiro de Software na Stone Age",
+      nome: "THIAGO FELDNER",
+      cargo: "Programador de Sistemas de Informação",
       imagem:
-        "https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg",
-      time: times[3].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "GUILHERME LIMA",
-      cargo: "Desenvolvedor Python e JavaScript na Alura",
-      imagem:
-        "	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg",
-      time: times[3].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "PAULO SILVEIRA",
-      cargo: "Hipster e CEO da Alura",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg",
-      time: times[3].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "JULIANA AMOASEI",
-      cargo: "Desenvolvedora de software e instrutora",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg",
+        "https://github.com/thiagofeldner.png",
       time: times[4].nome,
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: "DANIEL ARTINE",
-      cargo: "Engenheiro de Software na Stone Age",
+      nome: "THIAGO FELDNER",
+      cargo: "Programador de Sistemas de Informação",
       imagem:
-        "https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg",
-      time: times[4].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "GUILHERME LIMA",
-      cargo: "Desenvolvedor Python e JavaScript na Alura",
-      imagem:
-        "	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg",
-      time: times[4].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "PAULO SILVEIRA",
-      cargo: "Hipster e CEO da Alura",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg",
-      time: times[4].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "JULIANA AMOASEI",
-      cargo: "Desenvolvedora de software e instrutora",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg",
-      time: times[5].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "DANIEL ARTINE",
-      cargo: "Engenheiro de Software na Stone Age",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg",
-      time: times[5].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "GUILHERME LIMA",
-      cargo: "Desenvolvedor Python e JavaScript na Alura",
-      imagem:
-        "	https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg",
-      time: times[5].nome,
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: "PAULO SILVEIRA",
-      cargo: "Hipster e CEO da Alura",
-      imagem:
-        "https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg",
+        "https://github.com/thiagofeldner.png",
       time: times[5].nome,
     },
   ];
